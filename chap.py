ea2c5754bb9e246b5891f87a6dd32d4ed3a68a61
Belay <belayv@lski3661.mskcc.org>
2022-02-08 13:14:59 -0500

    update chap.py

--- chap.py
+++ chap.py
@@ -37,18 +37,133 @@ import os
 import sys
 import subprocess
 import getpass
-from scp import SCPclient, SCPException
+from scp import SCPClient, SCPException
 
 
-def do_chap(topology,trajectory,output_path):
+def do_chap(topology,trajectory,output_path,solvent='15'):
     
     def dcd_to_xtc(topology, trajectory):
         
         dcd_traj = md.load(trajectory,top=topology)
         dcd_traj.save_xtc(output_path+'/trajectory.xtc')
         
-        # Under development
+    host = 'hitegpu2'
+    username = 'hiter'
+    password = getpass.getpass("Enter password for "+username+'@'+host+":")
         
+    port = 22 
+        
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        
+    ssh.connect(host,port,username=username,password=password); x=1
+        
+    if x == 1:
+            
+        print('Connection to '+username+'@'+host+' successful')
+        
+        if trajectory.endswith('.dcd'):
+            print('Converting DCD to XTC...')
+            
+            dcd_to_xtc(topology, trajectory)
+            
+        elif trajectory.endswith('.xtc') == False:
+            
+            print('Please select a trajecory in either xtc or dcd format. Exiting.')
+            
+        else:
+            
+            print('Input trajectory is xtc, no conversion necessary.')
+            
+            
+        ############################################################    
+        # Create a temp dir in hitegpu2 for CHAP execution
+        stdin,stdout,stderr=ssh.exec_command('cd belayv ; mkdir vb_temp_chap_dir ;')
+        outlines1 = stdout.readlines()
+        outlines2 = stderr.readlines()
+        resp1=''.join(outlines1)
+        resp2=''.join(outlines2)
+        print(resp1)
+        print(resp2)
+        
+        ############################################################     
+            
+        
+        ############################################################
+        # Create SCP client and copy traj and topology to hitegpu
+        
+        traj_path = output_path+'/'+'equilibrated.xtc'
+        
+        scp=SCPClient(ssh.get_transport())
+        scp.put(topology,remote_path="/home/hiter/belayv/vb_temp_chap_dir/.")
+        scp.close()
+        print('Trajectory and topology have been temporarily copied to hitegpu2.')
+
+        
+        
+        
+        ############################################################
+
+
+
+        ############################################################
+        # Run CHAP execution
+        
+        print('Starting CHAP execution...')
+        
+        stdin,stdout,stderr=ssh.exec_command('cd belayv;cd vb_temp_chap_dir;source /programs/sbgrid.shrc;/programs/x86_64-linux/chap/0.9.1/bin.capsules/chap -f equilibrated.xtc -s step5_input.pdb -sel-pathway 1 -sel-solvent 15 -hydrophob-fallback -0.56846473')
+        outlines3 = stdout.readlines()
+        outlines4=stderr.readlines()
+        resp3=''.join(outlines3)
+        resp4=''.join(outlines4)
+        print(resp3)
+        print(resp4)
+        print('CHAP execution successful.')
+        
+        
+        ############################################################
+        
+        
+        ############################################################
+        # Copy output data from hitegpu2 back to original output dir
+        
+
+        scp = SCPClient(ssh.get_transport())
+        os.chdir(output_path)
+        
+        scp.get('/home/hiter/belayv/vb_temp_chap_dir/output.json')
+        scp.get('/home/hiter/belayv/vb_temp_chap_dir/output.pdb')
+        scp.get('/home/hiter/belayv/vb_temp_chap_dir/output.mtl')
+        
+        scp.close()
+        
+        print('Data transferred from hitegpu2 successfully.')
+        
+        
+        ############################################################
+
+
+
+        ############################################################
+        # Delete temp folder from hitehpu2
+        
+        stdin,stdout,stderr = ssh.exec_command('cd belayv;rm -r vb_temp_chap_dir')
+        outlines5=stdout.readline()
+        resp5=''.join(outlines5)
+        
+        print(resp5)
+        
+        ############################################################
+
+    ###
+    # Close SSH clinet
+    ssh.close()
+    print('SSH connection to '+username+'@'+host+' successfully closed.')
+    
+    
+    ###
+
+
 def load_data(data):
     
     with open(data) as data_file:

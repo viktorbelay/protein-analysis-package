4c019e183db82fe33f426193b776f0f5a6b737df
Belay <viktorbelay@gmail.com>
2023-06-20 16:34:19 -0400

    addition to join_lite

--- join_lite.py
+++ join_lite.py
@@ -17,9 +17,10 @@ import mdtraj as md
 import os
 import sys
 
-def join_lite(working_dir,stride_value=1,save_xtc='no'):
+def join_lite(working_dir,stride_value=1,n_frames_removed=0,save_xtc='no'):
 
     trajs = []
+    trajs_fr=[] # List for trajs with freames removed
 
     os.chdir(working_dir)
 
@@ -36,20 +37,28 @@ def join_lite(working_dir,stride_value=1,save_xtc='no'):
     for dcd in os.listdir(working_dir):
 
         if dcd.endswith('.dcd'):
+
             trajs.append(md.load(dcd,top=pdb_path,stride=stride_value))
 
+            if n_frames_removed != 0:
 
+                trajs_fr.append(md.load(dcd[n_frames_removed:],top=pdb_path,stride=stride_value))
 
 
     print(trajs)
     cat_traj=md.join(trajs)
+    cat_traj_fr=md.join(trajs_fr)
 
     if save_xtc=='no':
 
         cat_traj.save_dcd(working_dir+'/joined.dcd')
 
-        print(cat_traj,'has been saved as joined.dcd')
+        if n_frames_removed != 0:
+            cat_traj_fr.save_dcd(working_dir+'/joined_fr.dcd') # Save dcd with frames removed 
+            print(cat_traj_fr,'has been saved as joined_fr.dcd')
 
+        print(cat_traj,'has been saved as joined.dcd and ')
+        
     elif save_xtc=='yes':
 
         cat_traj.save_xtc(working_dir+'/joined.xtc')
@@ -58,7 +67,7 @@ def join_lite(working_dir,stride_value=1,save_xtc='no'):
 
 if __name__=='__main__':
 
-    join_lite(sys.argv[1],int(sys.argv[2]),sys.argv[3])
+    join_lite(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]),sys.argv[4])
 
 
 
